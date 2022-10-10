ff6fabfe2380354328426d851ddb75b7a6bf7d3a
Analia <analianovoa1985@gmail.com>
2022-10-09 23:48:38 -0300

    agregado de selectores y funcion elejir palabra secreta

--- script_game.js
+++ script_game.js
@@ -1,3 +1,20 @@
+const animales = ["LEOPARDO","HIPOPOTAMO","AVESTRUZ","ELEFANTE","JIRAFA","SERPIENTE","COCODRILO"];
+
+const lienzo = document.getElementById("ahorcado");		
+const contexto = lienzo.getContext("2d");
+
+let animalSecreto ="";
+
+function elejirAnimalSecreto(){
+        let animal = animales[Math.floor(Math.random() * animales.length)];
+        animalSecreto = animal;
+        console.log(animalSecreto);
+        return animal;
+}
+
 function iniciarJuego(){
-    document.getElementById("iniciar_juego").style.display = "none";
+    document.getElementById("btn-iniciar").style.display = "none";
+    elejirAnimalSecreto();
+    dibujarAhorcado();
+    cantidadErrores();
 }
